4237e35c4d5100fbfa24eba312e4eaeafd372285
amesri <abde_mes@yahoo.fr>
2020-01-19 22:24:27 +0100

    return Token to user after an success Login

--- DattingApp.API/DattingApp.API/Controllers/AuthController.cs
+++ DattingApp.API/DattingApp.API/Controllers/AuthController.cs
@@ -2,9 +2,14 @@
 using DattingApp.API.Dtos;
 using DattingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace DattingApp.API.Controllers
@@ -14,9 +19,11 @@ namespace DattingApp.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repos;
+        private readonly IConfiguration _config;
 
-        public AuthController(IAuthRepository repos)
+        public AuthController(IAuthRepository repos, IConfiguration config)
         {
+            _config = config;
             _repos = repos;
         }
 
@@ -38,6 +45,40 @@ namespace DattingApp.API.Controllers
             var createdUser = await _repos.Register(userToCreate, userForRegister.Password);
 
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _repos.Login(userForLoginDto.Username, userForLoginDto.Password);
+        
+            if (userFromRepo == null) 
+                Unauthorized();
+
+                     
+
+            var claims = new [] {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)      
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSetting:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+           var tokenHandler = new JwtSecurityTokenHandler();
+           var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new {
+                token = tokenHandler.WriteToken(token)
+            });
         }
 
     }
